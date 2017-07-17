54333fc4c8b7e986a68e14421da3c1da736c8843
apatheiagit <apatheiar@mail.ru>
2017-07-17 20:52:39 +0300

    blogs_new

--- templates/views-view-fields--blog.tpl.php
+++ templates/views-view-fields--blog.tpl.php
@@ -1,42 +1,47 @@
-<?php foreach ($fields as $id => $field):   
-    if($id == 'title') { $title = $field->content; }    
+<?php foreach ($fields as $id => $field): 
+    if($id == 'field_main_img') { $image = $field->content; }
+    if($id == 'title') { $title = $field->content; }
+    if($id == 'path') { $path = $field->content; }
+    if($id == 'body') { $body = $field->content; }    
+    if($id == 'field_city') { $city = $field->content; }
     if($id == 'field_title') { $title = $field->content; }
-    if($id == 'path') { $path = $field->content; }    
+    if($id == 'field_subtitle') { $subtitle = $field->content; }
     if($id == 'totalcount') { $totalcount = $field->content; }    
-    if($id == 'field_bloger') { $bloger_tid = $field->content; }    
  endforeach; ?>
+
 <?php 	
 	$theme_path = path_to_theme();
 	global $language_content; 
 	$lang = $language_content->language;
 	if ($lang == 'en') $prefix = '/en'; else $prefix = '';
- 	if (isset($bloger_tid)){
- 		$bloger = taxonomy_term_load($bloger_tid);
- 		$bloger_photo = $bloger->field_image['und'][0]['uri'];
- 		$bloger_name_localize = i18n_taxonomy_localize_terms($bloger);
- 		$bloger_name = $bloger_name_localize->name;	
+	if (isset($city)){
+ 		$term_city = taxonomy_term_load($city);
+ 		$city_name_localize = i18n_taxonomy_localize_terms($term_city);
+ 		$city_name = $city_name_localize->name;
+ 		if ($city == '134' && $lang == 'ru') $city_name = "Весь Кипр";
  	}
+ 	$titleLength = iconv_strlen($title, 'UTF-8');
 ?>
-<div class="col-sm-6 col-md-4 col-lg-3">	
-	<div class="blog-block">
-		<div class="photo">
-			<a href="<?php print $path; ?>">
-				<?php 
-	        $params = array(
-	          'style_name' => 'cyprus150x150',
-	          'path' => $bloger_photo,
-	          'alt' => $bloger_name,
-	          'attributes' => array('class' => array('img-circle', "img-responsive")),
-	          'getsize' => FALSE,
-	        );
-	        print theme('image_style', $params); ?>
-			</a>
-		</div>	
-		<div class="title-wrap">	
-			<div class="title"><?php print str_replace("/en/en", "/en", $title)?></div>
-		</div>
-		<div class="statistic">
-			<div class="metrika metrika-watch"><?php print file_get_contents($theme_path."/img/views.svg");?><span class="count"><?php print $totalcount;?></span></div>
+<div class="col-sm-4 media-wrapper--normal">
+<div class="media-block media-block--review media-block--blogs media-block--entertainment">
+	<div class="photo">		
+		<?php print $image;?>
+	</div>	
+	<div class="text">
+		<div class="category">		
+		<?php if(isset($city)):?>
+				<a href="<?php print $prefix;?>/reviews/<?php print $city;?>"><?php print $city_name; ?></a>
+		<?php else:?>
+				<a href="<?php print $prefix;?>/blog"><?php print t("Personal experience"); ?></a>
+		<?php endif;?>
 		</div>		
+		<div class="title"><a href="<?php print $path;?>"><?php print $title?></a></div>
+		<?php if ($titleLength < 46):?>
+			<div class="descr"><a href="<?php print $path;?>"><?php print $body;?></a></div>
+		<?php endif;?>
 	</div>
+	<div class="statistic">
+		<div class="metrika metrika-watch"><?php print file_get_contents($theme_path."/img/views.svg");?><span class="count"><?php print $totalcount;?></span></div>
+	</div>
+</div>
 </div>
