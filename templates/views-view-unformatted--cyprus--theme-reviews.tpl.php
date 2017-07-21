18257dd36f19c2f688da938fd7201f752724e65f
Petrova, Lidiia <Lidiia.Petrova@billing.ru>
2017-07-21 14:39:41 +0300

    small changes

---
+++ templates/views-view-unformatted--cyprus--theme-reviews.tpl.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php $counter = 0;?>
+<?php foreach ($rows as $id => $row): ?>
+	<?php if($counter < 3):?>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <?php print $row; ?>
+  </div>
+	<?php endif;?>
+	<?php $counter++;?>
+<?php endforeach; ?>
