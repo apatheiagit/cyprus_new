f9d1a3cfaecbfcdf07bf403645f501a4c2eff875
Petrova, Lidiia <Lidiia.Petrova@billing.ru>
2017-07-26 17:49:38 +0300

    pop reviews

--- templates/views-view-fields--main--pop-reviews.tpl.php
+++ templates/views-view-fields--main--pop-reviews.tpl.php
@@ -4,49 +4,69 @@
     if($id == 'path') { $path = $field->content; }
     if($id == 'body') { $body = $field->content; }
     if($id == 'field_section') { $section = $field->content; }
+    if($id == 'field_city') { $city = $field->content; }
     if($id == 'field_title') { $title = $field->content; }
     if($id == 'field_subtitle') { $subtitle = $field->content; }
     if($id == 'totalcount') { $totalcount = $field->content; }
     if($id == 'field_special') { $special = $field->content; }
-    if($id == 'field_rubtic') { $rubric = $field->content; }
+    if($id == 'field_type') { $type = $field->content; }    
+    if($id == 'field_specproekt') { $specproekt_tid = $field->content; }
  endforeach; ?>
 <?php 	
- 	global $language_content; 
-  	$lang = $language_content->language;
-  	if ($lang == 'en') $prefix = '/en'; else $prefix = '';
+	$theme_path = path_to_theme();
+	global $language_content; 
+	$lang = $language_content->language;
+	if ($lang == 'en') $prefix = '/en'; else $prefix = '';
+	if (isset($city)){
+ 		$term_city = taxonomy_term_load($city);
+ 		$city_name_localize = i18n_taxonomy_localize_terms($term_city);
+ 		$city_name = $city_name_localize->name;
+ 		if ($city == '134' && $lang == 'ru') $city_name = "Весь Кипр";
+ 	}
 	if (isset($section)){
  		$terms = taxonomy_term_load($section);
  		$english = $terms->field_english['und'][0]['value'];
- 		$russian_localize = i18n_taxonomy_localize_terms($terms);
- 		$russian = $russian_localize->name;
+ 		$russian_name_localize = i18n_taxonomy_localize_terms($terms);
+ 		$russian = $russian_name_localize->name;
  	}
- 	if (isset($rubric)){
- 		$term = taxonomy_term_load($rubric);
- 		$rubric_type = $term->field_english['und'][0]['value'];		
+ 	if (isset($specproekt_tid)){
+ 		$specproekt_term = taxonomy_term_load($specproekt_tid);	
+ 		$specproekt_name_localize = i18n_taxonomy_localize_terms($specproekt_term);
+ 		$specproekt = $specproekt_name_localize->name;	
  	}
+ 	$titleLength = iconv_strlen($title, 'UTF-8');
 ?>
-<div class="col-st-6 col-sm-6 col-md-3">
-	<div class="article-item article-item--common article-item--plain">
-		<div class="article-photo-wrap <?php if(isset($rubric_type)):?>article-photo--rubric<?endif;?>">
-		<?php if(isset($rubric_type)):?>
-		<div class="article-rubric article-rubric--<?php print $english?>">
-			<a href="<?php print $path; ?>" class="fixed-height"><span class="article-canvas article-canvas--<?php print $rubric_type;?>"></span></a>
-		</div>
-	<?php else:?>
-	<div class="article-photo">		
+<div class="col-sm-4 media-wrapper--normal">		
+<div class="media-block media-block--review media-block--pop-review  media-block--<?php print $type;?> media-block--<?php print $english;?>">
+	<div class="photo">		
 		<?php print $image;?>
+	</div>	
+	<div class="text">
+		<?php if($type == 'photo'):?>
+			<a class="photo-icon" href="<?php print $prefix;?>/photoreviews"></a>
+		<?php endif;?>
+		<div class="category">
+		<?php if($special == 1):?>
+			<?php if(isset($specproekt_tid)):?>
+				<a href="<?php print $prefix;?>/special/<?php print $specproekt_tid;?>"><?php print $specproekt;?></a>
+			<?php else:?>
+				<a href="<?php print $prefix;?>/special"><?php print t("Special project"); ?></a>
+			<?php endif;?>
+		<?php elseif($type == 'lifehack'):?>
+				<a href="<?php print $prefix;?>/lifehack"><?php print t("Life hack"); ?></a>
+		<?php elseif(isset($city)):?>
+				<a href="<?php print $prefix;?>/reviews/<?php print $city;?>"><?php print $city_name; ?></a>
+		<?php else:?>
+				<a href="<?php print $prefix;?>/<?php print $english;?>"><?php print $russian; ?></a>
+		<?php endif;?>
+		</div>		
+		<div class="title"><a href="<?php print $path;?>"><?php print $title?></a></div>
+		<?php if ($titleLength < 43):?>
+			<div class="descr"><a href="<?php print $path;?>"><?php print $body;?></a></div>
+		<?php endif;?>
 	</div>
-	<?php endif;?>
-		<div class="article-text">
-			<div class="article-type article-type--<?php print $english?>"><a class="type-text" href="<?php print $prefix;?>/<?php print $english;?>"><?php print $russian; ?></a></div>
-			<div class="article-title-descr">
-				<div class="article-title"><?php print str_replace("/en/en", "/en", $title)?></div>
-				<div class="article-descr"><?php print str_replace("/en/en", "/en", $body)?></div>
-			</div>
-			<div class="article-stat">
-				<div class="stat stat-watch"><span class="icon icon-views"></span><span class="count"><?php print $totalcount;?></span></div>
-			</div>
-		</div>
-		</div>
+	<div class="statistic">
+		<div class="metrika metrika-watch"><?php print file_get_contents($theme_path."/img/views.svg");?><span class="count"><?php print $totalcount;?></span></div>
 	</div>
+</div>
 </div>
