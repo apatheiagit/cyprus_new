4ff0afd504d60d7781094597b0be116ab8df66e9
Petrova, Lidiia <Lidiia.Petrova@billing.ru>
2018-06-30 00:43:51 +0300

    cousine bloger

---
+++ templates/views-view-unformatted--blog--video.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<div class="blog-wrap--<?php print count($rows);?>">
+<?php foreach ($rows as $id => $row): ?>  
+    <?php print $row; ?>  
+<?php endforeach; ?>
+</div>
